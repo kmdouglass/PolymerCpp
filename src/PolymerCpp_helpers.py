b835b803ac0d5fd6cef086b8b8f443816648913c
MStefko <marcel.stefko1@gmail.com>
2016-03-21 21:39:13 +0100

    Added comments.

--- src/PolymerCpp_helpers.py
+++ src/PolymerCpp_helpers.py
@@ -1,6 +1,7 @@
 import PolymerCpp
 import numpy as np
 
+# These 2 functions return a chain with specified parameters in simulation units.
 def getCppWLC(pathLength = 1000.0,\
 			  linDensity = 1.0,\
 			  persisLength = 1.0,\
@@ -22,7 +23,10 @@ def getCppSAWLC(pathLength = 1000.0,\
 	return np.reshape(rawChain, (-1,3))
 
 
-def getCppWLCradii(numPaths = 10,\
+# These 2 functions return the gyration radius, you can get multiple values at once,
+# but not for different values of pathLength at the same time. If you need a range of values
+# of pathLength, just call this function N number of times with numpaths = 1.
+def getCppWLCradii(numPaths = 1,\
 				pathLength = 1000.0,\
 			  linDensity = 1.0,\
 			  persisLength = 1.0,\
@@ -38,7 +42,7 @@ def getCppWLCradii(numPaths = 10,\
 			i+=1
 		return radii[i+1:-1] #return all values after the negative one
 
-def getCppSAWLCradii(numPaths = 10,\
+def getCppSAWLCradii(numPaths = 1,\
 				pathLength = 1000.0,\
 			  linDensity = 1.0,\
 			  persisLength = 1.0,\
